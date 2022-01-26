c33de5f2cd186d94bd7fd5e1a951cbf028051d09
soumya <soumyanema03@gmail.com>
2022-01-26 22:48:55 +0530

    created current room context with usecontext selector

--- src/components/chat-window/top/index.jsx
+++ src/components/chat-window/top/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useCurrentRoom } from '../../../context/current-room.context';
 
 const Top = () => {
-  return <div>top</div>;
+  const name = useCurrentRoom(v => v.name);
+  return <div>{name}</div>;
 };
 
 export default Top;
