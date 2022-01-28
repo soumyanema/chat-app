95f7144142d24778a2a67bce7c81f57399db1f8c
soumya <soumyanema03@gmail.com>
2022-01-28 20:49:15 +0530

    displayed messages inside chat page

---
+++ src/components/chat-window/messages/messageItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Timeago from 'timeago-react';
+import ProfileAvatar from '../../dashboard/ProfileAvatar';
+
+const messageItem = ({ message }) => {
+  const { author, createdAt, text } = message;
+  return (
+    <li className="padded mb-1">
+      <div className="d-flex align-items-center font-bolder mb-1">
+        <ProfileAvatar
+          src={author.avatar}
+          name={author.name}
+          className="ml-1"
+          size="xs"
+        />
+        <span className="ml-2">{author.name}</span>
+        <Timeago
+          datetime={createdAt}
+          className="text-black-45 font-normal ml-2"
+        />
+      </div>
+      <div className="word-break-all">{text}</div>
+    </li>
+  );
+};
+
+export default messageItem;
