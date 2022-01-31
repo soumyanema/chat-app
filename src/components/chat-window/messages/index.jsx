106dc26fb99056b5689423e28c1c7ef6f97b630d
soumya <soumyanema03@gmail.com>
2022-01-31 16:19:55 +0530

    handled delete message operation

--- src/components/chat-window/messages/index.jsx
+++ src/components/chat-window/messages/index.jsx
@@ -49,6 +49,37 @@ const Messages = () => {
     [chatId]
   );
 
+  const handleDelete = useCallback(
+    async msgId => {
+      // eslint-disable-next-line no-alert
+      if (!window.confirm('Delete this message?')) {
+        return;
+      }
+      const isLast = messages[messages.length - 1].id === msgId;
+
+      const updates = {};
+
+      updates[`/messages/${msgId}`] = null;
+
+      if (isLast && messages.length > 1) {
+        updates[`rooms/${chatId}/lastMessage`] = {
+          ...messages[messages.length - 2],
+          msgId: messages[messages.length - 2].id,
+        };
+      }
+      if (isLast && messages.length === 1) {
+        updates[`rooms/${chatId}/lastMessage`] = null;
+      }
+      try {
+        await database.ref().update(updates);
+        Alert.info('Message deleted', 4000);
+      } catch (err) {
+        Alert.error(err.message, 4000);
+      }
+    },
+    [chatId, messages]
+  );
+
   const handleLike = useCallback(async msgId => {
     const { uid } = auth.currentUser;
     const messageRef = database.ref(`/messages/${msgId}`);
@@ -85,6 +116,7 @@ const Messages = () => {
             message={msg}
             handleAdmin={handleAdmin}
             handleLike={handleLike}
+            handleDelete={handleDelete}
           />
         ))}
     </ul>
