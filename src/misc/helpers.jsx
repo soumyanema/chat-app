4e72dbef8e3077432debd771c31062c52201e87a
soumya <soumyanema03@gmail.com>
2022-02-02 16:42:00 +0530

    grouped messages by date

--- src/misc/helpers.jsx
+++ src/misc/helpers.jsx
@@ -73,3 +73,16 @@ export function useHover() {
 
   return [ref, value];
 }
+
+export function groupBy(array, groupingKeyFn) {
+  return array.reduce((result, item) => {
+    const groupingKey = groupingKeyFn(item);
+
+    if (!result[groupingKey]) {
+      result[groupingKey] = [];
+    }
+    result[groupingKey].push(item);
+
+    return result;
+  }, {});
+}
