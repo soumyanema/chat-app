580a1925b675369f3f5ba06e0d7b4bb99b0fb225
soumya <soumyanema03@gmail.com>
2022-02-02 16:06:03 +0530

    displayed audio messages and managed deletion of audio files

--- src/components/chat-window/messages/index.jsx
+++ src/components/chat-window/messages/index.jsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
-import { auth, database } from '../../../misc/firebase';
+import { auth, database, storage } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
 
@@ -50,7 +50,7 @@ const Messages = () => {
   );
 
   const handleDelete = useCallback(
-    async msgId => {
+    async (msgId, file) => {
       // eslint-disable-next-line no-alert
       if (!window.confirm('Delete this message?')) {
         return;
@@ -74,7 +74,17 @@ const Messages = () => {
         await database.ref().update(updates);
         Alert.info('Message deleted', 4000);
       } catch (err) {
-        Alert.error(err.message, 4000);
+        // eslint-disable-next-line consistent-return
+        return Alert.error(err.message, 4000);
+      }
+
+      if (file) {
+        try {
+          const fileRef = storage.refFromURL(file.url);
+          await fileRef.delete();
+        } catch (err) {
+          Alert.error(err.message, 4000);
+        }
       }
     },
     [chatId, messages]
