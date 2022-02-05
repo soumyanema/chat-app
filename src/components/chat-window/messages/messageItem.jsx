151b34193dbbb64d2943c1da817089aca3858728
soumya <soumyanema03@gmail.com>
2022-02-05 15:39:36 +0530

    .

--- src/components/chat-window/messages/messageItem.jsx
+++ src/components/chat-window/messages/messageItem.jsx
@@ -19,6 +19,16 @@ const renderFileMessage = file => {
       </div>
     );
   }
+
+  if (file.contentType.includes('audio')) {
+    return (
+      // eslint-disable-next-line jsx-a11y/media-has-caption
+      <audio controls>
+        <source src={file.url} type="audio/mp3" />
+        Your browser doesnot support audio element
+      </audio>
+    );
+  }
   return <a href={file.url}>Download {file.name}</a>;
 };
 
@@ -81,7 +91,7 @@ const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
             isVisible={canShowIcons}
             iconName="trash"
             toolTip="Delete this message"
-            onClick={() => handleDelete(message.id)}
+            onClick={() => handleDelete(message.id, file)}
           />
         )}
       </div>
